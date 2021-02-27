edc0fbc140b0ff7536a310d969f5830e63eb783c
Harder74 <tylerdrewharder@ksu.edu>
2021-02-27 11:14:31 -0600

    Player Movement
    Started player movement

---
+++ GameJam21/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void ProcessInputs()
+    {
+        //float moveX =   
+    }
+}
