268b92a79bbe5e52b4818d533d260ac049d84624
Harder74 <tylerdrewharder@ksu.edu>
2021-02-27 14:49:03 -0600

    Player Flipping

--- GameJam21/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ GameJam21/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -33,11 +33,20 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDir = new Vector2(moveX, moveY);
+
     }
 
     void Move()
     {
         rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+        if(moveDir.x > 0)
+        {
+            rb.transform.localScale = new Vector3(-.3f, rb.transform.localScale.y, 1);
+        }
+        else if(moveDir.x < 0)
+        {
+            rb.transform.localScale = new Vector3(.3f, rb.transform.localScale.y, 1);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
