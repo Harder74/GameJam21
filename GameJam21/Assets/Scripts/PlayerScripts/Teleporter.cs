09934501779268dbf077f7e1c94aeccf86fa58a6
AndrewDulac <54514013+AndrewDulac@users.noreply.github.com>
2021-02-27 12:42:25 -0600

    tele clean now

---
+++ GameJam21/Assets/Scripts/PlayerScripts/Teleporter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject sight;
+    [SerializeField]
+    private GameObject player;
+    private GameObject tempsight;
+    private bool place;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            place = !place;
+            if (place)
+            {  
+                tempsight = GameObject.Instantiate(sight, player.transform.position, Quaternion.identity);
+            }
+            else
+            {
+                player.transform.position = tempsight.transform.position;
+                GameObject.DestroyImmediate(tempsight);
+            }
+        }
+    }
+}
